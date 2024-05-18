f8c1049d19291d577c001cbb71da26ed44db3b0b
unknown <gajendra.rathore.dev07@gmail.com>
2024-05-18 14:21:58 +0530

    Added the logic of proxy in the frontend app

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -21,7 +21,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/mcAd.jpg"
+        src="/api/assets/mcAd.jpg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
